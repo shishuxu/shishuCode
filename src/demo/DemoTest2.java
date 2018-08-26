331c78163b6d02d8cc398dcf356dc76919c44562
shishuxu <xu457683048@163.com>
2018-08-26 15:51:40 +0800

    test2

--- src/demo/DemoTest2.java
+++ src/demo/DemoTest2.java
@@ -1,8 +1,16 @@
 package demo;
 
+import java.text.SimpleDateFormat;
+
 public class DemoTest2 {
 
     public static void main(String[] args) {
-        System.out.println(6729);
+        System.out.println(64444729);
+        long time = System.currentTimeMillis();
+        System.out.println(time);
+        SimpleDateFormat dateFormat =  new SimpleDateFormat("yyyy");
+        System.out.println(dateFormat.format(time));
+
+
     }
 }
